b70dcbb536c59ac6b3ec0a89ff1c94d012d194e1
Lovish Bajaj <bajaj.lovish@gmail.com>
2016-12-06 04:03:57 +0530

    Revert "new commit with error"
    This reverts commit e01125dfebb7533f10fe3ab6ee7f7679bf836514.

--- app/src/main/java/com/app/meenabazaar/meenabazaar/MainActivity.java
+++ app/src/main/java/com/app/meenabazaar/meenabazaar/MainActivity.java
@@ -8,35 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.PropertyInfo;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-
 public class MainActivity extends AppCompatActivity {
 
     Toolbar mActionBarToolbar;
     Button newProduct;
-    Button createInvoice;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         newProduct = (Button) findViewById(R.id.addproduct);
-        createInvoice=(Button) findViewById(R.id.createInvoice);
         newProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(), Invoice.class);
-                startActivity(i);
-            }
-        });
-        createInvoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(), Article_Activity.class);
+                Intent i = new Intent(getBaseContext(), NewProduct_activity.class);
                 startActivity(i);
             }
         });
